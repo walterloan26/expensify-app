511d5065f672de7016c70c142fd3ba6589c98476
walterloan26 <loanfigueiredo@gmail.com>
2020-01-21 17:46:38 -0600

    Added firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,9 +6,9 @@ const firebaseConfig = {
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-    appId: FIREBASE_APP_ID,
-    measurementId: FIREBASE_MEASUREMENT_ID
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
   };
 
   firebase.initializeApp(firebaseConfig);
