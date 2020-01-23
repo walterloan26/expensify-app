1c7d868f260231978f85aebd9e7078c50af4f19a
walterloan26 <loanfigueiredo@gmail.com>
2020-01-22 18:48:14 -0600

    store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -9,7 +9,8 @@ export const addExpense = (expense) => ({
  
 
 export const startAddExpense = (expenseData = {}) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
     const {
       description = '',
       note = '',
@@ -17,7 +18,7 @@ export const startAddExpense = (expenseData = {}) => {
       createdAt = 0
     } = expenseData;
     const expense = { description, note, amount, createdAt };
-    database.ref('expenses').push(expense).then((ref) => {
+    database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
@@ -33,8 +34,9 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 export const startRemoveExpenses = ({ id } = {}) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).remove().then((snapshot) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).remove().then((snapshot) => {
       dispatch(removeExpense({ id }))
     })
   }
@@ -48,8 +50,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpenses = (id, updates) => {
-  return (dispatch) => {
-    return database.ref(`expenses/${id}`).update(updates).then(() => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
       dispatch(editExpense( id, updates))
     })
   }
@@ -63,8 +66,9 @@ export const setExpenses = (expenses) => ({
 
 // export const startSetExpenses // firebase
 export const startSetExpenses = () => {
-  return (dispatch) => {
-    return database.ref('expenses').once('value').then((snapshot) => {
+  return (dispatch, getState) => {
+    const uid = getState().auth.uid;
+    return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
       const expenses = [];
 
       snapshot.forEach((childSnapshot) => {
--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
-import Header from '../components/Header'
 
-export const PrivateRoute = ({
+export const PublicRoute = ({
     isAuthenticated, 
-    component: Component, 
+    component: Component,
     ...rest
 }) => (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
-            <div>
-                <Header />
-                <Component {...props}/>
-            </div>  
+            <Redirect to="/dashboard" />
         ) : (
-            <Redirect to="/" />
+            <Component {...props} />
         )
     )} />
 );
-
-
-
 
 const mapStateToProps = (state) => ({
     isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute)
+export default connect(mapStateToProps)(PublicRoute)
