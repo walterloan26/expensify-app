527d072fd5f60b02921a63cd4c91e614dc5184a0
walterloan26 <loanfigueiredo@gmail.com>
2020-01-23 14:26:16 -0600

    Final deployment

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,7 +8,7 @@ export const LoginPage = ({ startLogin }) => {
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control.</p>
-            <button onClick={startLogin} className="button"> Login with google </button>
+            <button onClick={startLogin} className="button"> Login with Google </button>
         </div>
          
         </div>
